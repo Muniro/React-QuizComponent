eef94833d0a55a82e1234b4aa09c37c8dc3874b4
Munir Syed <MunirSyed@bandce.co.uk>
2022-01-06 12:46:57 +0000

    Initia changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import reactDom from 'react-dom/cjs/react-dom.development'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+
+<App />, 
+document.getElementById('root'))
 registerServiceWorker()
